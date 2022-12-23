a4a732fafc31effc7b4eb0eaace54b70ac5046f8
Osyfliak Ivan <osuflak22@gmail.com>
2022-12-23 19:22:49 +0200

    зробив перше завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const imgList = galleryItems
+
+    .map(({ preview, original, description }) =>
+    
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+
+    .join("");
+
+
+gallery.insertAdjacentHTML("beforeend", imgList);
+    
+gallery.addEventListener('click', onGalleryImgClick);
+
+function onGalleryImgClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+
+    } else {
+
+const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+    
+    } 
+}
