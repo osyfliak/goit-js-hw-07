6de84c087e01ae23d028499d80ea2b9457b11167
Osyfliak Ivan <osuflak22@gmail.com>
2022-12-23 20:31:34 +0200

    2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+const imgList = galleryItems
+
+    .map(({ preview, original, description }) =>
+    
+        `<a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" /></a>`
+    )
+
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", imgList);
+    
+
+gallery.addEventListener('click', onGalleryImgClick);
+
+function onGalleryImgClick(event) {
+    event.preventDefault();
+   
+    let lightbox = new SimpleLightbox('.gallery a', { 
+    
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+        
+     });
+}
+
